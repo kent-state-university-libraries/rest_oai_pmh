443f45ca4bc658a8d7a1398705bdfab6d66b94ce
Seth Shaw <seth.shaw@unlv.edu>
2019-11-07 13:43:07 -0800

    fix oai_dc:dc attributes

--- src/Plugin/OaiMetadataMap/DublinCoreRdf.php
+++ src/Plugin/OaiMetadataMap/DublinCoreRdf.php
@@ -32,6 +32,9 @@ class DublinCoreRdf extends OaiMetadataMapBase
         return [
           'oai_dc' => [
             '@xmlns:dc' => 'http://purl.org/dc/elements/1.1/',
+            '@xmlns:oai_dc' => 'http://www.openarchives.org/OAI/2.0/oai_dc/',
+            '@xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
+            '@xsi:schemaLocation' => 'http://www.openarchives.org/OAI/2.0/oai_dc/ http://www.openarchives.org/OAI/2.0/oai_dc.xsd',
           ],
         ];
     }
